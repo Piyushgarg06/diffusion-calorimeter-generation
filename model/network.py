22abc28da712249eb356575e12fb2da075fa895d
Piyushgarg06 <erpiyushgarg17@gmail.com>
2026-03-29 07:16:37 +0530

    feat: add CNN-based noise predictor with timestep embedding and dilated convolutions

--- model/network.py
+++ model/network.py
@@ -1,35 +1,48 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class SimpleCNN(nn.Module):
-
     def __init__(self, T=200):
-
         super().__init__()
-
+        self.T = T
         self.time_embed = nn.Embedding(T, 32)
 
-        self.conv1 = nn.Conv2d(8, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.conv3 = nn.Conv2d(64,32,3,padding=1)
-        self.conv4 = nn.Conv2d(32,8,3,padding=1)
+        self.t_proj_32 = nn.Linear(32, 32)
+        self.t_proj_64 = nn.Linear(32, 64)
 
-        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(10, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+
+        self.conv3 = nn.Conv2d(64, 64, 3, padding=2, dilation=2)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=4, dilation=4)
+
+        self.conv5 = nn.Conv2d(64, 32, 3, padding=1)
+        self.conv6 = nn.Conv2d(32, 8, 3, padding=1)
 
     def forward(self, x, t):
+        B = x.shape[0]
 
-        t_emb = self.time_embed(t)
+        data = x[:, :8]
+        coords = x[:, 8:]
+        total_energy = data.sum(dim=(1, 2, 3))
+        total_energy = total_energy / (total_energy.mean() + 1e-6)
+        total_energy = total_energy.view(B, 1, 1, 1)
+        x = torch.cat([data, coords], dim=1)
 
-        t_emb = t_emb[:, :, None, None]
+        t_emb = self.time_embed(t)                          
+        t32 = self.t_proj_32(t_emb).view(B, 32, 1, 1)
+        t64 = self.t_proj_64(t_emb).view(B, 64, 1, 1)
+        x1 = F.relu(self.conv1(x))
 
-        h = self.relu(self.conv1(x))
+        x = F.relu(self.conv2(x1 + t32))     
+        x = F.relu(self.conv3(x + t64))      
+        x = F.relu(self.conv4(x + t64))      
 
-        h = h + t_emb
+        x = F.relu(self.conv5(x))          
 
-        h = self.relu(self.conv2(h))
-        h = self.relu(self.conv3(h))
+        x = x + x1                          
 
-        out = torch.tanh(self.conv4(h))
-
-        return out
+        x = self.conv6(x)                    
+        return x
